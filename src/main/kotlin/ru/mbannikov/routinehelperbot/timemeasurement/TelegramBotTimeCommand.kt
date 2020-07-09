a0cdbc1df25d62defa179eed7efbcc9c292553af
Maksim Bannikov <spystieber@gmail.com>
2020-07-09 10:58:18 +0300

    Рефакторинг класса TelegramBotTimeCommand: вынес все тексты сообщений в отдельный helper класс

--- src/main/kotlin/ru/mbannikov/routinehelperbot/timemeasurement/TelegramBotTimeCommand.kt
+++ src/main/kotlin/ru/mbannikov/routinehelperbot/timemeasurement/TelegramBotTimeCommand.kt
@@ -15,7 +15,7 @@ import kotlin.reflect.KFunction1
 class TelegramBotTimeCommand(
     private val measureTimeLogRepository: MeasureTimeLogRepository
 ) : TelegramBotCommandWithArgs, Logging {
-    override val command: String = "timer"
+    override val command: String = COMMAND
 
     override val handler: CommandHandleUpdate = { bot, update, args ->
         try {
@@ -25,7 +25,7 @@ class TelegramBotTimeCommand(
         } catch (e: Throwable) {
             log.error(e) { "Unknown error" }
             val message = "Произошла неизвестная ошибка: ${e.message ?: e.javaClass.simpleName}"
-            bot.sendMessage(chatId = update.message!!.chat.id, text = message)
+            sendMessage(bot, update, message)
         }
     }
 
@@ -47,7 +47,7 @@ class TelegramBotTimeCommand(
         log.info { "got command \"/$command $START_ACTION\"" }
 
         timeMeasurer.start()
-        bot.sendMessage(chatId = update.message!!.chat.id, text = "Таймер запущен")
+        sendMessage(bot, update, text = "Таймер запущен")
     }
 
     private fun handleFinishAction(bot: Bot, update: Update) {
@@ -57,13 +57,12 @@ class TelegramBotTimeCommand(
         val todayDuration = measureTimeLogRepository.getTodayDuration()
         val thisWeekDuration = measureTimeLogRepository.getThisWeekDuration()
 
-        val responseMessage = buildFinishActionMessage(
+        val responseMessage = MessageBuilder.buildFinishActionMessage(
             now = duration,
             today = todayDuration,
             thisWeek = thisWeekDuration
         )
-
-        bot.sendMessage(chatId = update.message!!.chat.id, text = responseMessage)
+        sendMessage(bot, update, responseMessage)
     }
 
     private fun handleMinusAction(bot: Bot, update: Update, args: List<String>) {
@@ -88,79 +87,96 @@ class TelegramBotTimeCommand(
         val duration = Duration.ofMinutes(minutes)
 
         handler(duration)
-
-        val responseMessage = buildStatisticMessage()
-        bot.sendMessage(chatId = update.message!!.chat.id, text = responseMessage, parseMode = ParseMode.MARKDOWN)
+        sendStatisticMessage(bot, update)
     }
 
     private fun handleStatAction(bot: Bot, update: Update) {
         log.info { "got command \"/$command $STAT_ACTION\"" }
 
-        val responseMessage = buildStatisticMessage()
-        bot.sendMessage(chatId = update.message!!.chat.id, text = responseMessage, parseMode = ParseMode.MARKDOWN)
+        sendStatisticMessage(bot, update)
+    }
+
+    private fun sendStatisticMessage(bot: Bot, update: Update) {
+        val todayDuration = measureTimeLogRepository.getTodayDuration()
+        val thisWeekDuration = measureTimeLogRepository.getThisWeekDuration()
+
+        val responseMessage = MessageBuilder.buildStatisticMessage(todayDuration, thisWeekDuration)
+        sendMessage(bot, update, responseMessage)
     }
 
     private fun sendHelpMessage(bot: Bot, update: Update) {
         log.info { "got command \"/$command\"" }
 
-        val responseMessage = """*Начать замер*:
-            |/$command $START_ACTION
-            |
-            |*Закончить замер*: 
-            |/$command $FINISH_ACTION
-            |
-            |*Отнять продолжительность минут* (на случай, если забыл завершить замер):
-            |/$command $MINUS_ACTION %MIN%
-            |
-            |*Прибавить продолжительность минут* (на случай, если замер не делался):
-            |/$command $PLUS_ACTION %MIN%
-            |
-            |*Показать статистику за день и текущую неделю:*
-            |/$command $STAT_ACTION
-        """.trimMargin()
-
-        bot.sendMessage(chatId = update.message!!.chat.id, text = responseMessage, parseMode = ParseMode.MARKDOWN)
+        val responseMessage = MessageBuilder.buildHelpMessage()
+        sendMessage(bot, update, responseMessage)
     }
 
-    private fun buildFinishActionMessage(now: Duration, today: Duration, thisWeek: Duration): String =
-        try {
-            val nowSpentTime = buildSpentTimeString(now)
-            val todaySpentTime = buildSpentTimeString(today)
-            val thisWeekSpentTime = buildSpentTimeString(thisWeek)
-
-            """Сейчас: $nowSpentTime
-                |В течении дня: $todaySpentTime
-                |В течении недели: $thisWeekSpentTime
-            """.trimMargin()
-        } catch (e: TimerWasNotStarted) {
-            "Ранее таймер не был запущен."
-        }
-
-    private fun buildStatisticMessage(): String {
-        val todaySpentTime = measureTimeLogRepository.getTodayDuration().let(::buildSpentTimeString)
-        val thisWeekSpentTime = measureTimeLogRepository.getThisWeekDuration().let(::buildSpentTimeString)
-
-        return """В течении дня: $todaySpentTime
-            |В течении недели: $thisWeekSpentTime
-        """.trimMargin()
-    }
-
-    private fun buildSpentTimeString(duration: Duration): String {
-        val durationInMinutes = duration.toMinutes()
-        val hours = durationInMinutes / 60
-        val minutes = durationInMinutes - (hours * 60)
-
-        return when (hours) {
-            0L -> "$minutes мин."
-            else -> "$hours ч. $minutes мин."
-        }
+    private fun sendMessage(bot: Bot, update: Update, text: String) {
+        bot.sendMessage(chatId = update.message!!.chat.id, text = text, parseMode = ParseMode.MARKDOWN)
     }
 
     companion object {
-        private const val START_ACTION = "start"
-        private const val FINISH_ACTION = "finish"
-        private const val MINUS_ACTION = "minus"
-        private const val PLUS_ACTION = "plus"
-        private const val STAT_ACTION = "stat"
+        internal const val COMMAND = "timer"
+        internal const val START_ACTION = "start"
+        internal const val FINISH_ACTION = "finish"
+        internal const val MINUS_ACTION = "minus"
+        internal const val PLUS_ACTION = "plus"
+        internal const val STAT_ACTION = "stat"
+    }
+}
+
+private class MessageBuilder private constructor() {
+    companion object {
+        private const val command: String = TelegramBotTimeCommand.COMMAND
+
+        fun buildHelpMessage() = """*Начать замер*:
+            |/$command ${TelegramBotTimeCommand.START_ACTION}
+            |
+            |*Закончить замер*: 
+            |/$command ${TelegramBotTimeCommand.FINISH_ACTION}
+            |
+            |*Отнять продолжительность минут* (на случай, если забыл завершить замер):
+            |/$command ${TelegramBotTimeCommand.MINUS_ACTION} %MIN%
+            |
+            |*Прибавить продолжительность минут* (на случай, если замер не делался):
+            |/$command ${TelegramBotTimeCommand.PLUS_ACTION} %MIN%
+            |
+            |*Показать статистику за день и текущую неделю:*
+            |/$command ${TelegramBotTimeCommand.STAT_ACTION}
+        """.trimMargin()
+
+        fun buildFinishActionMessage(now: Duration, today: Duration, thisWeek: Duration): String =
+            try {
+                val nowSpentTime = buildSpentTimeString(now)
+                val todaySpentTime = buildSpentTimeString(today)
+                val thisWeekSpentTime = buildSpentTimeString(thisWeek)
+
+                """Сейчас: $nowSpentTime
+                |В течении дня: $todaySpentTime
+                |В течении недели: $thisWeekSpentTime
+            """.trimMargin()
+            } catch (e: TimerWasNotStarted) {
+                "Ранее таймер не был запущен."
+            }
+
+        fun buildStatisticMessage(todayDuration: Duration, thisWeekDuration: Duration): String {
+            val todaySpentTime = buildSpentTimeString(todayDuration)
+            val thisWeekSpentTime = buildSpentTimeString(thisWeekDuration)
+
+            return """В течении дня: $todaySpentTime
+                |В течении недели: $thisWeekSpentTime
+            """.trimMargin()
+        }
+
+        fun buildSpentTimeString(duration: Duration): String {
+            val durationInMinutes = duration.toMinutes()
+            val hours = durationInMinutes / 60
+            val minutes = durationInMinutes - (hours * 60)
+
+            return when (hours) {
+                0L -> "$minutes мин."
+                else -> "$hours ч. $minutes мин."
+            }
+        }
     }
 }
